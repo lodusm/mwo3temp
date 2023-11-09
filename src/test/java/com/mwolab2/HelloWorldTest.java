14a38b16733d5217cb60b55ba9fe3aa0c16c2dd4
Maciej Sudoł <lodus@Maciejs-MacBook-Air.local>
2023-11-09 12:55:11 +0100

    proba1

---
+++ src/test/java/com/mwolab2/HelloWorldTest.java
@@ -0,0 +1,27 @@
+package com.mwolab2;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+
+    @Test
+    void testAverage() {
+        HelloWorld helloWorld = new HelloWorld();
+        double result = helloWorld.average(3.0, 5.0);
+        assertEquals(4.0, result, 0.0001); // Using delta for double comparison
+    }
+
+    @Test
+    void testIsEven() {
+        HelloWorld helloWorld = new HelloWorld();
+
+        assertTrue(helloWorld.isEven(2));
+        assertFalse(helloWorld.isEven(3));
+        assertTrue(helloWorld.isEven(0));
+        assertFalse(helloWorld.isEven(-1));
+    }
+}
+
+
